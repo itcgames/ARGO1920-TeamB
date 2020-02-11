3d5578876bfe3525b1ad8dfd512f0128002b1b9e
patryk wysocki <c00213855@itcarlow.ie>
2020-02-11 16:24:31 +0000

    Player restart position when collided with spike

--- ARGO_Team_B/ARGO_Team_B/SRC/ControlComponent.cpp
+++ ARGO_Team_B/ARGO_Team_B/SRC/ControlComponent.cpp
@@ -27,25 +27,25 @@ void ControlComponent::handleInput()
 			if (m_controller->m_currentState.DpadUp || m_controller->m_currentState.LeftThumbStick.y < -m_controller->dpadThreshold) {
 				p_walkUp = new WalkUpCommand();
 				p_walkUp->execute(m_entity);
-				renderComp->setAngle(270);
+				//renderComp->setAngle(270);
 			}
 
 			if (m_controller->m_currentState.DpadLeft || m_controller->m_currentState.LeftThumbStick.x < -m_controller->dpadThreshold) {
 				p_walkLeft = new WalkLeftCommand();
 				p_walkLeft->execute(m_entity);
-				renderComp->setAngle(180);
+				//renderComp->setAngle(180);
 			}
 
 			if (m_controller->m_currentState.DpadRight || m_controller->m_currentState.LeftThumbStick.x > m_controller->dpadThreshold) {
 				p_walkRight = new WalkRightCommand();
 				p_walkRight->execute(m_entity);
-				renderComp->setAngle(0);
+				//renderComp->setAngle(0);
 			}
 
 			if (m_controller->m_currentState.DpadDown || m_controller->m_currentState.LeftThumbStick.y > m_controller->dpadThreshold) {
 				p_walkDown = new WalkDownCommand();
 				p_walkDown->execute(m_entity);
-				renderComp->setAngle(90);
+				//renderComp->setAngle(90);
 			}
 		}
 		else if (!playerComp->getMoveable()) {
