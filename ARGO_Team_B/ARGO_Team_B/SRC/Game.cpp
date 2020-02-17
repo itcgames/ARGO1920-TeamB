1a275fc000059d65ac9db9b406e20b36da1acd78
Eoin <c002217717@itcarlow.ie>
2020-02-17 10:25:45 +0000

    added rotation

--- ARGO_Team_B/ARGO_Team_B/SRC/Game.cpp
+++ ARGO_Team_B/ARGO_Team_B/SRC/Game.cpp
@@ -46,7 +46,7 @@ Game::Game()
 	m_player.addComponent(new PlayerComponent(1), Types::Player); // This must allways be first added
 	m_player.addComponent(new HealthComponent(100), Types::Health);
 	m_player.addComponent(new PositionComponent(150, 100), Types::Position);
-	m_player.addComponent(new CollisionComponent(m_player, 60.0f, RAT_H, RAT_W), Types::Collider);
+	m_player.addComponent(new CollisionComponent(m_player, 50.0f, RAT_H, RAT_W), Types::Collider);
 	m_player.addComponent(new ControlComponent(m_player), Types::Controller);
 	m_player.addComponent(new RenderComponent("./Assets/rat.png", RAT_W, RAT_H, p_renderer), Types::Render);
 	//m_player.addComponent(new AnimatedSpriteComponent("./Assets/SpriteSheet.png", 60, 30, 5, p_renderer), Types::AnimatedSprite);
