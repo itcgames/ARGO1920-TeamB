9df19675d142984e71470d1ae885e78d17ebff34
patryk wysocki <c00213855@itcarlow.ie>
2020-02-17 11:57:43 +0000

    menu to be created

---
+++ ARGO_Team_B/ARGO_Team_B/INCLUDE/Scene.h
@@ -0,0 +1,20 @@
+//#ifndef SCENE_H
+//#define SCENE_H
+//#include <SDL.h>
+//#include <vector>
+//
+//class Scene
+//{
+//	public:
+//		Scene();
+//		~Scene();
+//		void handleInput(SDL_Event t_event);
+//		virtual void draw();
+//		virtual void update();
+//	private:
+//		SDL_Renderer* m_renderer;
+//		SDL_Window* m_window;
+//		float m_width, m_height;
+//};
+//#endif // !SCENE_H
+//
