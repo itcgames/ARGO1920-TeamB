27e11a812b508268cca0e14d67ba5e3fab7a7646
Dion Buckley <dion-b@hotmail.com>
2020-02-19 11:47:47 +0000

    Test Bot framework

---
+++ ARGO_Team_B/ARGO_Team_B/INCLUDE/Components/TestBotBehaviourComponent.h
@@ -0,0 +1,36 @@
+#ifndef TEST_BOT_BEHAVIOUR_COMPONENT_H
+#define TEST_BOT_BEHAVIOUR_COMPONENT_H
+
+#include "AISystem.h"
+#include "Component.h"
+#include "Command.h"
+
+#include "StateMachineSystem.h"
+#include "Enums.h"
+
+#include "PositionComponent.h"
+#include "PlayerComponent.h"
+#include "Xbox360Controller.h"
+#include "Entity.h"
+
+class TestBotBehaviourComponent : public Component
+{
+public:
+	TestBotBehaviourComponent(Entity& t_gameObject);
+	~TestBotBehaviourComponent();
+
+private:
+
+	Entity& m_entity;
+
+	/// <summary>
+	/// Commands
+	/// </summary>
+	Command* p_walkUp;
+	Command* p_walkDown;
+	Command* p_walkLeft;
+	Command* p_walkRight;
+	Command* p_interact;
+};
+
+#endif // !TEST_BOT_BEHAVIOUR_COMPONENT_H
---
+++ ARGO_Team_B/ARGO_Team_B/SRC/Components/TestBotBehaviourComponent.cpp
@@ -0,0 +1,11 @@
+#include "TestBotBehaviourComponent.h"
+
+TestBotBehaviourComponent::TestBotBehaviourComponent(Entity& t_gameObject) :
+	m_entity{ t_gameObject }
+{
+}
+
+TestBotBehaviourComponent::~TestBotBehaviourComponent()
+{
+}
+
